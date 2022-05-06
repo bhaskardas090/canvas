d28c4b747c9f641a48288cad2b11b76f2beb5a36
Bhaskar Das <bhaskardas090@gmail.com>
2022-05-06 17:58:11 +0530

    canvas int

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { Stage, Layer, Image, Text } from 'react-konva';
+import useImage from 'use-image';
+// import Video from './Video';
+import { Html } from 'react-konva-utils';
 
-function App() {
+const LionImage = () => {
+  const [image] = useImage(
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTDkP7cWIBGzvnpvXC8R9rZW4nmzpbNjbXV6F7y7DAzA5gBtIFBSgPRCo7sAFsABalfTo&usqp=CAU'
+  );
+  return <Image image={image} />;
+};
+export default function App() {
+  const [textContent, setTextContent] = useState('Some text on canvas');
+  const [showText, setShowText] = useState(true);
+  const handleText = () => {
+    setShowText((prevS) => !prevS);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <LionImage />
+          {showText && <Text text={textContent} fontSize={25} padding={300} />}
+          <Html
+            divProps={{
+              style: {
+                // position: 'absolute',
+              },
+            }}
+          >
+            <video width="750" height="500" controls className="video">
+              <source src="mov_bbb.mp4" type="video/mp4" />
+            </video>
+            <button className="textbutton" onClick={handleText}>
+              Hide Text
+            </button>
+          </Html>
+        </Layer>
+      </Stage>
     </div>
   );
 }
-
-export default App;
---
+++ src/Video.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Video = () => {
+  return (
+    <video width="750" height="500" controls>
+      <source src="mov_bbb.mp4" type="video/mp4" />
+    </video>
+  );
+};
+
+export default Video;
